44945162434a0188430356fc8e535c58e85e8051
Matt S.Y. Ho <methodho@gmail.com>
2017-05-08 20:46:42 +0800

    Update

--- src/main/java/tw/com/softleader/security/xss/json/jackson/XSSStringDeserializer.java
+++ src/main/java/tw/com/softleader/security/xss/json/jackson/XSSStringDeserializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -16,12 +17,11 @@ public class XSSStringDeserializer extends StdScalarDeserializer<String> {
 
   private static final long serialVersionUID = -1184343455367339303L;
   private final Function<String, String> escaper;
-  private final StdScalarDeserializer<String> delegate;
+  private final StringDeserializer delegate = StringDeserializer.instance;
 
   public XSSStringDeserializer(Function<String, String> escaper) {
     super(String.class);
     this.escaper = escaper;
-    delegate = new StringDeserializer();
   }
 
   @Override
@@ -30,5 +30,17 @@ public class XSSStringDeserializer extends StdScalarDeserializer<String> {
     String escaped = Optional.ofNullable(before).map(escaper::apply).orElse(before);
     log.debug("Escaped field '{}' from [{}] to [{}]", p.getCurrentName(), before, escaped);
     return escaped;
+  }
+
+  @Override
+  public String deserializeWithType(
+      JsonParser jp, DeserializationContext ctxt, TypeDeserializer typeDeserializer)
+      throws IOException {
+    return delegate.deserializeWithType(jp, ctxt, typeDeserializer);
+  }
+
+  @Override
+  public boolean isCachable() {
+    return delegate.isCachable();
   }
 }
