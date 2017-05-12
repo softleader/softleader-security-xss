5093506980ddcbe33979cd8f7142f425f80a77b0
Matt S.Y. Ho <methodho@gmail.com>
2017-05-12 12:18:21 +0800

    Update

--- src/main/java/tw/com/softleader/security/xss/http/CoverityXSSProtectionFilter.java
+++ src/main/java/tw/com/softleader/security/xss/http/CoverityXSSProtectionFilter.java
@@ -2,17 +2,10 @@ package tw.com.softleader.security.xss.http;
 
 import com.coverity.security.Escape;
 
-import java.util.function.Function;
-
 /** @author Matt S.Y Ho */
 public class CoverityXSSProtectionFilter extends XSSProtectionFilter {
 
   public CoverityXSSProtectionFilter() {
     super(Escape::htmlText);
-  }
-
-  @Override
-  public final void setEscaper(Function<String, String> escaper) {
-    throw new UnsupportedOperationException("Use XSSProtectionFilter to customize escaper");
   }
 }
