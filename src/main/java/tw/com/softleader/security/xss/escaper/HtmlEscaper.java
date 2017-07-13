3f68928f1edd853e2ec2762a6413f208cbb1148b
Matt S.Y. Ho <methodho@gmail.com>
2017-07-13 11:38:40 +0800

    Update

--- src/main/java/tw/com/softleader/security/xss/escaper/HtmlEscaper.java
+++ src/main/java/tw/com/softleader/security/xss/escaper/HtmlEscaper.java
@@ -5,6 +5,8 @@ import org.springframework.web.util.HtmlUtils;
 import java.util.function.Function;
 
 /**
+ * 使用 Spring 的 {@link HtmlUtils#htmlEscape(String)} 來跳脫
+ *
  * @author Matt S.Y Ho
  */
 public class HtmlEscaper implements Function<String, String> {
--- src/main/java/tw/com/softleader/security/xss/http/HtmlXSSProtectionFilter.java
+++ src/main/java/tw/com/softleader/security/xss/http/HtmlXSSProtectionFilter.java
@@ -2,12 +2,11 @@ package tw.com.softleader.security.xss.http;
 
 import tw.com.softleader.security.xss.escaper.HtmlEscaper;
 
-import java.util.function.Function;
-
 /**
+ * @see HtmlEscaper
  * @author Matt S.Y Ho
  */
-public class HtmlXSSProtectionFilter extends XSSProtectionFilter{
+public class HtmlXSSProtectionFilter extends XSSProtectionFilter {
 
   public HtmlXSSProtectionFilter() {
     super(new HtmlEscaper());
